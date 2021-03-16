9861b28418a29b3e49530821e4888d5d1d4d3d57
X407403 <alvaro.de.la.chica.fernandez@everis.com>
2021-03-16 13:54:52 +0100

    producs path

--- src/front-tech-app-shell.ts
+++ src/front-tech-app-shell.ts
@@ -17,6 +17,71 @@ const applications = constructApplications({
 });
 
 const layoutEngine = constructLayoutEngine({ routes, applications });
+// applications.forEach((app) => {
+//   if (app.name === "@front-tech/inspire") {
+//     return registerApplication({...app,
+//       customProps: {
+//         title: "RECOMMENDATIONS",
+//         items: `[
+//         {​​​​​
+//         "id": 1,
+//         "name": "Tractor Blue",
+//         "url": "https://front-tech.github.io/products/",
+//         "img": "https://raw.githubusercontent.com/neuland/micro-frontends/master/0-model-store/images/tractor-blue-thumb.jpg"
+//         }​​​​​,
+//         {​​​​​
+//         "id": 2,
+//         "name": "Tractor Green",
+//         "url": "https://front-tech.github.io/products/",
+//         "img": "https://raw.githubusercontent.com/neuland/micro-frontends/master/0-model-store/images/tractor-green-thumb.jpg"
+//         }​​​​​,
+//         {​​​​​
+//         "id": 3,
+//         "name": "Tractor Red",
+//         "url": "https://front-tech.github.io/products/",
+//         "img": "https://raw.githubusercontent.com/neuland/micro-frontends/master/0-model-store/images/tractor-red-thumb.jpg"
+//         }​​​​​]`,
+//       }
+//     });
+//   } else {
+//     return registerApplication;
+//   }
+// });
+
+// applications.forEach((conf) => {
+//   const mfeCustomConfig =
+//     conf.name === "@front-tech/inspire"
+//       ? {
+//           customProps: {
+//             title: "RECOMMENDATIONS",
+//             items: JSON.stringify([
+//               {
+//                 id: 1,
+//                 name: "Tractor Blue",
+//                 url: "https://front-tech.github.io/products/",
+//                 img:
+//                   "https://raw.githubusercontent.com/neuland/micro-frontends/master/0-model-store/images/tractor-blue-thumb.jpg",
+//               },
+//               {
+//                 id: 2,
+//                 name: "Tractor Green",
+//                 url: "https://front-tech.github.io/products/",
+//                 img:
+//                   "https://raw.githubusercontent.com/neuland/micro-frontends/master/0-model-store/images/tractor-green-thumb.jpg",
+//               },
+//               {
+//                 id: 3,
+//                 name: "Tractor Red",
+//                 url: "https://front-tech.github.io/products/",
+//                 img:
+//                   "https://raw.githubusercontent.com/neuland/micro-frontends/master/0-model-store/images/tractor-red-thumb.jpg",
+//               },
+//             ]),
+//           },
+//         }
+//       : {};
+//   registerApplication({ ...conf, ...mfeCustomConfig });
+// });
 applications.forEach(registerApplication);
 layoutEngine.activate();
 start();
