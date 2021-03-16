08ce2651dd1d440e96f0088eb235637ed1923541
X407403 <alvaro.de.la.chica.fernandez@everis.com>
2021-03-16 11:13:06 +0100

    working example mf

--- src/front-tech-app-shell.ts
+++ src/front-tech-app-shell.ts
@@ -8,12 +8,14 @@ import {
 const routes = constructRoutes(
   document.querySelector("#single-spa-layout") as HTMLTemplateElement
 );
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
 });
+
 const layoutEngine = constructLayoutEngine({ routes, applications });
 applications.forEach(registerApplication);
 layoutEngine.activate();
